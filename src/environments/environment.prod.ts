e2f78e304a0d75a2cc836eb0e3200aaa2587011b
Rajkumar Murugesan <rajkumarmurugesan66@gmail.com>
2026-05-09 12:48:07 +0530

    url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://date-monitor-backend-production.up.railway.app'
+  apiUrl: 'https://date-monitor-backend-production.up.railway.app/api'
 };
